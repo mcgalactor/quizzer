785f4b4a40b6fcad0872366458e13e3a54086ae2
Oktay <oktay@yatko.dev>
2021-09-28 16:43:48 +0200

    Refactor Popup with Tailwind

--- client/src/components/PopUp.js
+++ client/src/components/PopUp.js
@@ -14,10 +14,12 @@ const PopUp = () => {
   };
 
   return (
-    <div className="overlay show">
-      <div className="popup">
-        <span className="title">{title}</span>
-        <span className="message">{message}</span>
+    <div className="fixed inset-0 flex flex-col items-center justify-center bg-opacity-70 bg-black">
+      <div className="bg-qlight flex flex-col items-center justify-center text-center space-y-6 p-8 w-80 border-qlight rounded-md">
+        <div className="flex flex-col space-y-4">
+          <span className="text-qdark font-semibold uppercase text-3xl">{title}</span>
+          <span className="text-qdark font-semibold text-lg">{message}</span>
+        </div>
         <Button type="small" onClick={handleClick}>
           {button}
         </Button>
