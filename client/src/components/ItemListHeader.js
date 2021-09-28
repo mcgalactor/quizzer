abebb19b32fda520c992198e63c8f7c4cc2f554a
Oktay <oktay@yatko.dev>
2021-09-28 15:47:33 +0200

    Fix ItemListHeader font size

--- client/src/components/ItemListHeader.js
+++ client/src/components/ItemListHeader.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const ItemListHeader = ({ children, ...rest }) => {
-  return <h2 {...rest}>{children}</h2>;
+  return (
+    <h2 {...rest} className="p-1 py-4 text-2xl">
+      {children}
+    </h2>
+  );
 };
 
 export default ItemListHeader;
