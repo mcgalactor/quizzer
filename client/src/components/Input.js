c86aa75119fa294bf12ed1542f187372b5074690
Oktay <oktay@yatko.dev>
2021-09-28 15:12:50 +0200

    Refactor Input with Tailwind

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import clsx from 'clsx';
+
 import { useSelector, useDispatch } from 'react-redux';
 import { textInputHandlerAction } from '../reducers/team-app';
 
@@ -15,9 +17,11 @@ const Input = ({
   const value = useSelector(state => state[reducer][item].value);
   const dispatch = useDispatch();
 
-  const handleChange = () => ({ target: { value } }) => {
-    dispatch(textInputHandlerAction(item, value, minLength, maxLength, uppercase));
-  };
+  const handleChange =
+    () =>
+    ({ target: { value } }) => {
+      dispatch(textInputHandlerAction(item, value, minLength, maxLength, uppercase));
+    };
 
   return (
     <>
@@ -28,8 +32,13 @@ const Input = ({
         </label>
       )}
       <input
+        className={clsx(
+          'w-full h-16 text-qgrey bg-qlight font-extrabold text-xl py-1 px-4 outline-none',
+          'transition ease-in-out duration-200',
+          'border-qgrey border-2 border-r-2 focus:border-qorange',
+          'normal-case'
+        )}
         id={item}
-        style={{ textTransform: uppercase ? 'uppercase' : null }}
         placeholder={placeholder}
         value={value}
         onChange={handleChange()}
