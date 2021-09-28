9d22a493c1913c303fd3fa5bc1496a72e964dbd6
Oktay <oktay@yatko.dev>
2021-09-28 21:36:32 +0200

    Fix logo size (enables Tailwind JIT mode)

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 
 const Logo = center => (
-  <h1 className={clsx('font-badaboombb text-8xl select-none', center && 'text-center')}>
+  <h1 className={clsx('font-badaboombb text-[6.5rem] select-none', center && 'text-center')}>
     Quizzer!
   </h1>
 );
