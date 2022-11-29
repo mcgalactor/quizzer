e03a9dfbc980f4d8149c071f940219beb665f416
mcgalactor <github@janhanne.de>
2022-11-29 00:22:33 -0500

    release2

--- server/mongo/docker-entrypoint-initdb.d/en_questions.js
+++ server/mongo/docker-entrypoint-initdb.d/en_questions.js
@@ -1,20 +1,57 @@
 db.questions.insertMany([
 	{
-		"question": "Saint Bruno Bus",
+		"question": "Which bus line stops directly in the front of the Alstom Office",
 		"questiontype": "multi",
-		"options" : ["Ja","nein","vielleicht","auf jeden fAll"],
-		"answer": 1,
+		"options" : ["96","98","94","99"],
+		"answer": 2,
 		"category": "Alstom",
 		"language": "en"
 	}
 	,
 	{
-		"question": "Saint Bruno Cantina",
+		"question": "What is the price for a meal in the cafeteria?",
 		"questiontype": "multi",
 		"options" : ["Ja","nein","vielleicht","auf jeden fAll"],
 		"answer": 2,
 		"category": "Alstom",
 		"language": "en"
+	},
+
+	{
+		"question": "How many employees work for Alstom worldwide?",
+		"questiontype": "multi",
+		"options" : ["68.000","86.000","74.000","92.000"],
+		"answer": 3,
+		"category": "Alstom",
+		"language": "en"
+	},
+
+	{
+		"question": "What's the distance between the Alstom offices in Saint-Bruno and Kingston (shortest distance on the road)",
+		"questiontype": "multi",
+		"options" : ["334 km","289 km","381 km","401 km"],
+		"answer": 1,
+		"category": "Alstom",
+		"language": "en"
+	},
+
+	{
+		"question": "Which country does France share its longest border with?",
+		"questiontype": "multi",
+		"options" : ["Germany","Belgium","Brazil","Spain"],
+		"answer": 3,
+		"category": "Alstom",
+		"language": "en"
+	},
+	{
+		"question": "What is the name of the song?",
+		"questiontype": "multi",
+		"options" : ["Symphony of destruction","Master of Puppets","Don't Stop Believin'","666 - The Number of the Beast"],
+		"answer": 1,
+		"category": "Alstom",
+		"language": "en"
 	}
+
+
 
 ])
